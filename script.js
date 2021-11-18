8734cdc05460af14215147d261b3992ad257fb85
SukirtiSaxena <sukirtisaxena@gmail.com>
2021-11-18 21:10:07 +0000

    working password checker

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 document.querySelector(".strengthMeter").style.width = 0;
 
 function textValueChanged() {
-    const num = (document.getElementById('password').value.length);
-    document.querySelector(".strengthMeter").style.width = (num * 5) + "%";
-    strengthMeterBarColor(num*5);
+    const num = document.querySelector(".password").value.length;
+    const text = document.querySelector(".feedback");
+    document.querySelector(".strengthMeter").style.width = (num * 5) + "px";
+    strengthMeterBarColor(num * 5, text);
 }
 
-function strengthMeterBarColor(num) {
-    console.log("num",num);
+function strengthMeterBarColor(num, text) {
     if (num < 40) {
         document.querySelector(".strengthMeter").style.background = "red";
-        }
+        text.innerHTML = "Password should be longer";
+    }
     else if (num < 60) {
         document.querySelector(".strengthMeter").style.background = "orange";
-        }
-    else if (num > 60) {      
+        text.innerHTML = "Pretty good";
+    }
+    else if (num > 60) {
         document.querySelector(".strengthMeter").style.background = "green";
-     }
+        text.innerHTML = "Grrrreat!";
+    }
 }
 
-    
