00e5b0978e148bb034971831f48a69464b387571
SukirtiSaxena <sukirtisaxena@gmail.com>
2021-11-18 19:26:31 +0000

    strength meter working on password length

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+document.querySelector(".strengthMeter").style.width = 0;
 
 function textValueChanged() {
-    alert('Changed');
+    const num = (document.getElementById('password').value.length);
+    document.querySelector(".strengthMeter").style.width = (num * 5) + "%";
+    strengthMeterBarColor(num*5);
 }
+
+function strengthMeterBarColor(num) {
+    console.log("num",num);
+    if (num < 40) {
+        document.querySelector(".strengthMeter").style.background = "red";
+        }
+    else if (num < 60) {
+        document.querySelector(".strengthMeter").style.background = "orange";
+        }
+    else if (num > 60) {      
+        document.querySelector(".strengthMeter").style.background = "green";
+     }
+}
+
+    
